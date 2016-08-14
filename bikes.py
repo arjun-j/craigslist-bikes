9be47472a6a1aaace5cd756f67aa519b2d782d60
Arjun J <ajagdis@ncsu.edu>
2016-08-14 14:54:11 -0400

    pushbullet
    used pushbullet api to push notifications

--- bikes.py
+++ bikes.py
@@ -1,43 +1,48 @@
 import json
 import smtplib
 import time
+from pushbullet import Pushbullet
 from craigslist import CraigslistForSale
 
-def sendmsg(server,result):
-	number='<number>@tmomail.net'
-	msg=result['name'] + ' costs ' + result['price']+' posted on ' + result['datetime'] + 'url:  '+result['url']
-	server.sendmail( '<from>', str(number), str(msg) )
-
+api_key='o'
+pb= Pushbullet(api_key)
+my_channel=pb.channels[0]
 postlist=[]
-server = smtplib.SMTP( "smtp.gmail.com", 587 )
-server.starttls()
-server.login( '<gmail_address>', '<gmail_password>' )
 
-try:
-	json_data=open('bikelist.json').read()
-	data = json.loads(json_data)
-	for post in data:
-		postlist.append(post)
-except:
-	print "first run"
+def sendmsg(pb,result):
+	msg=result['name'] + ' costs ' + result['price']+' posted on ' + result['datetime'] 
+	push=pb.push_link(str(msg),str(result['url']))
+	push1=my_channel.push_link(str(msg),str(result['url']))
+	print "Message sent!"
+	print push
 
-craig = CraigslistForSale(site='raleigh',category='bia',filters={ 'max_price':60, 'min_price':15, 'has_image':True}	)
-#craig = CraigslistForSale(site='raleigh',category='bia',filters={ 'max_price':60, 'min_price':5, 'has_image':True, 'posted_today':True}	)
-results=craig.get_results(sort_by='newest',limit=18)
+while true:
+	try:
+		json_data=open('bikelist.json').read()
+		data = json.loads(json_data)
+		for post in data:
+			postlist.append(post)
+	except:
+		print "first run"
 
-for result in results:
-	comp=False
-	#print result
-	for item in postlist:
-		if (item['url'] == result['url']):
-			comp=True
-			break
-	print result['where']
-	if((comp==False) and (result['where']=='Raleigh')):
-		postlist.append(result)
-		print result
+	#craig = CraigslistForSale(site='raleigh',category='bia',filters={ 'max_price':60, 'min_price':15, 'has_image':True}	)
+	craig = CraigslistForSale(site='raleigh',category='bia',filters={ 'max_price':60, 'min_price':16, 'has_image':True, 'posted_today':True}	)
+	results=craig.get_results(sort_by='newest',limit=20)
 
-with open('bikelist.json', 'w') as outfile:
-	jstring=json.dumps(postlist)
-	outfile.write(jstring)
-#time.sleep(5*3600)
+	for result in results:
+		comp=False
+		#print result
+		for item in postlist:
+			if (item['url'] == result['url']):
+				comp=True
+				break
+		print result['where']
+		if((comp==False) and ( (result['where']=='Raleigh/Cary Crossroads Cary Area') or result['where']=='Raleigh')):
+			postlist.append(result)
+			sendmsg(pb,result)
+			print result
+
+	with open('bikelist.json', 'w') as outfile:
+		jstring=json.dumps(postlist)
+		outfile.write(jstring)
+	time.sleep(8*3600)
