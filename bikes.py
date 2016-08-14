3aca79a32f654d43c64a80768cc41b2a33638c01
Arjun J <ajagdis@ncsu.edu>
2016-08-14 00:58:54 -0400

    json changes

--- bikes.py
+++ bikes.py
@@ -1,12 +1,19 @@
 import json
 from craigslist import CraigslistForSale
 
+
 craig = CraigslistForSale(site='raleigh',category='bia',filters={ 'max_price':60, 'min_price':15, 'has_image':True}	)
 #craig = CraigslistForSale(site='raleigh',category='bia',filters={ 'max_price':60, 'min_price':5, 'has_image':True, 'posted_today':True}	)
 results=craig.get_results(sort_by='newest',limit=3)
-
+postlist=[]
 for result in results:
 	print result
-	with open('bikelist.txt', 'a') as outfile:
-		json.dump(result, outfile)
+	postlist.append(result)
+with open('bikelist.txt', 'a') as outfile:
+	json.dumps(postlist, outfile)
 
+
+
+json_data=open('bikelist.txt').read()
+
+data = json.loads(json_data)
