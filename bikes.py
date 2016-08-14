492ca107b400886780e43621a075b6332c4340ef
Arjun J <ajagdis@ncsu.edu>
2016-08-14 13:35:22 -0400

    sms
    added sms support

--- bikes.py
+++ bikes.py
@@ -1,7 +1,18 @@
 import json
+import smtplib
+import time
 from craigslist import CraigslistForSale
 
+def sendmsg(server,result):
+	number='<number>@tmomail.net'
+	msg=result['name'] + ' costs ' + result['price']+' posted on ' + result['datetime'] + 'url:  '+result['url']
+	server.sendmail( '<from>', str(number), str(msg) )
+
 postlist=[]
+server = smtplib.SMTP( "smtp.gmail.com", 587 )
+server.starttls()
+server.login( '<gmail_address>', '<gmail_password>' )
+
 try:
 	json_data=open('bikelist.json').read()
 	data = json.loads(json_data)
@@ -12,20 +23,21 @@ except:
 
 craig = CraigslistForSale(site='raleigh',category='bia',filters={ 'max_price':60, 'min_price':15, 'has_image':True}	)
 #craig = CraigslistForSale(site='raleigh',category='bia',filters={ 'max_price':60, 'min_price':5, 'has_image':True, 'posted_today':True}	)
-results=craig.get_results(sort_by='newest',limit=3)
+results=craig.get_results(sort_by='newest',limit=18)
 
 for result in results:
-	print result
 	comp=False
+	#print result
 	for item in postlist:
-		print item['url']
 		if (item['url'] == result['url']):
 			comp=True
 			break
-	if(comp==False):
+	print result['where']
+	if((comp==False) and (result['where']=='Raleigh')):
 		postlist.append(result)
+		print result
 
 with open('bikelist.json', 'w') as outfile:
 	jstring=json.dumps(postlist)
 	outfile.write(jstring)
-
+#time.sleep(5*3600)
