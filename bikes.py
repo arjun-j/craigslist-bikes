2376c25e19d2cf524f6ca977d15f8e4e402bcda0
Arjun J <ajagdis@ncsu.edu>
2016-08-14 02:56:08 -0400

    fixed son writing problem
    works fine now, need to add constraints

--- bikes.py
+++ bikes.py
@@ -1,23 +1,31 @@
 import json
 from craigslist import CraigslistForSale
 
+postlist=[]
+try:
+	json_data=open('bikelist.json').read()
+	data = json.loads(json_data)
+	for post in data:
+		postlist.append(post)
+except:
+	print "first run"
 
 craig = CraigslistForSale(site='raleigh',category='bia',filters={ 'max_price':60, 'min_price':15, 'has_image':True}	)
 #craig = CraigslistForSale(site='raleigh',category='bia',filters={ 'max_price':60, 'min_price':5, 'has_image':True, 'posted_today':True}	)
 results=craig.get_results(sort_by='newest',limit=3)
-postlist=[]
+
 for result in results:
 	print result
-	postlist.append(result)
+	comp=False
+	for item in postlist:
+		print item['url']
+		if (item['url'] == result['url']):
+			comp=True
+			break
+	if(comp==False):
+		postlist.append(result)
 
+with open('bikelist.json', 'w') as outfile:
+	jstring=json.dumps(postlist)
+	outfile.write(jstring)
 
-print postlist	
-
-with open('bikelist.json', 'a') as outfile:
-	j=json.dumps(postlist)
-	outfile.write(j)
-
-
-json_data=open('bikelist.json').read()
-
-data = json.loads(json_data)
